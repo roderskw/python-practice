af974f6dc814305e99890e55590b3ee6af43fe34
roderskw <rodrigokwakami@hotmail.com>
2022-10-27 00:47:37 -0300

    1 chama

--- Contains duplicate/contains duplicate.py
+++ Contains duplicate/contains duplicate.py
@@ -2,3 +2,4 @@ def containsDuplicate(self, nums: list[int]) -> bool:
     if(len(set(nums)) == len(nums)):
         return False
     return True
+# set() method is used to convert any of the iterable to sequence of iterable elements with distinct elements
